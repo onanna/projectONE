f21a839fb4c3f0a6925f1cc8876eb7b69723dd6a
james <jjoseph6@gmail.com>
2018-10-22 19:03:03 -0400

    working copy 1

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,203 @@
 // A $( document ).ready() block. Code waiting for document to load (doucment ready jquery)
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+var subscriptionKey = "709f3fa6b4694c208fcb0c00e0215bc9"
+var Gkey = "AIzaSyB7TE4cFhhyLquzYnqgpAKY9USJcOf4ErA"
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+today = mm + '-' + dd + '-' + yyyy;
+
+
+
+    $(function() {
+    var params = {
+        // Request parameters
+    };
+    
+    $.ajax({
+        url: "https://api.fantasydata.net/v3/nba/scores/JSON/GamesByDate/" + today + $.param(params),
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+        type: "GET",
+        // Request body
+        data: "{body}",
+    })
+    .done(function(data) {
+        console.log("TODAY'S GAMES & SCORES success");
+        console.log(data);
+
+  
+
+
+
+
+
+
+    })
+    .fail(function() {
+        console.log("error");
+    });
+    });
+
+
+
+    // $(function() {
+    // var params = {
+    //     // Request parameters
+    // };
+    
+    // $.ajax({
+    //     url: "https://api.fantasydata.net/v3/nba/scores/JSON/Games/2019?" + $.param(params),
+    //     beforeSend: function(xhrObj){
+    //         // Request headers
+    //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+    //     },
+    //     type: "GET",
+    //     // Request body
+    //     data: "{body}",
+    // })
+    // .done(function(data) {
+    //     console.log("SZN SCHEDULE success");
+    //     console.log(data);
+    // })
+    // .fail(function() {
+    //     alert("error");
+    // });
+    // });
+
+
+
+
+    // $(function() {
+    // var params = {
+    //     // Request parameters
+    // };
+    
+    // $.ajax({
+    //     url: "https://api.fantasydata.net/v3/nba/scores/{format}/Standings/2019?" + $.param(params),
+    //     beforeSend: function(xhrObj){
+    //         // Request headers
+    //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+    //     },
+    //     type: "GET",
+    //     // Request body
+    //     data: "{body}",
+    // })
+    // .done(function(data) {
+    //     console.log("STANDINGS success");
+    //     console.log(data);
+    // })
+    // .fail(function() {
+    //     console.log("error");
+    // });
+    // });
+
+
+
+
+    // $(function() {
+    // var params = {
+    //     // Request parameters
+    // };
+    
+    // $.ajax({
+    //     url: "https://api.fantasydata.net/v3/nba/scores/{format}/Stadiums?" + $.param(params),
+    //     beforeSend: function(xhrObj){
+    //         // Request headers
+    //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key",subscriptionKey);
+    //     },
+    //     type: "GET",
+    //     // Request body
+    //     data: "{body}",
+    // })
+    // .done(function(data) {
+    //     console.log("TEAM STADIUM success");
+    //     console.log(data);
+    // })
+    // .fail(function() {
+    //     console.log("error");
+    // });
+    // });
+
+
+
+
+    $(function() {
+
+    var params = {
+        // Request parameters
+    };
+    
+    $.ajax({
+        url: "https://api.fantasydata.net/v3/nba/scores/JSON/AllTeams?" + $.param(params),
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+
+        type: "GET",
+        // Request body
+        data: "{body}",
+    })
+
+    .done(function(data) {
+        console.log("All TEAMS LIST success");
+        console.log(data);
+        
+
+        for(var i = 0; i < 30; i++){
+            var name = "<div class='name'>" + (data[i].Name) + " </div>";
+            var image = name + '<img src=' + (data[i].WikipediaLogoUrl) + ">"
+            image = '<div class="col-md-2">' + image + "</div>";
+            $('#images').append(image);
+        }
+
+    })
+    .fail(function() {
+        console.log("error");
+    });
+    });
+
+
+    $(function() {
+    var params = {
+        // Request parameters
+    };
+    
+    $.ajax({
+        url: "https://api.fantasydata.net/v3/nba/scores/JSON/AreAnyGamesInProgress?" + $.param(params),
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+        type: "GET",
+        // Request body
+        data: "{body}",
+    })
+    .done(function(data) {
+        console.log("CURRENT GAMES success");
+        console.log(data);
+    })
+    .fail(function() {
+        console.log("error");
+    });
+    });
+
+
+
+
+
+
+
+
+
