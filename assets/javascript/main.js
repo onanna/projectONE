a7994aa04e59bda182a930fb879914b59cfa162a
james <jjoseph6@gmail.com>
2018-10-24 21:34:03 -0400

    aps working , somewhat

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,6 +3,12 @@ var subscriptionKey = "709f3fa6b4694c208fcb0c00e0215bc9"
 var Gkey = "AIzaSyB7TE4cFhhyLquzYnqgpAKY9USJcOf4ErA"
 var placeKey = "AIzaSyBFwGqbtcoa0X8yvd6XpX3P6A8tBC6iB7g"
 
+// var nbaLat = ""
+// var nbaLong = 
+
+
+
+
 //variables to get current date
 
 var today = new Date();
@@ -46,7 +52,7 @@ var yyyy = today.getFullYear();
         // var scorer = ticker + '<div class="board" "  data-team="'+ data[i].Name +'">'
         // scorer = '<div class="col-md-4">' + scorer + "</div>";
         $('#todayScores').append(ticker);
-        console.log(ticker)
+       
         
 
 
@@ -124,29 +130,102 @@ var yyyy = today.getFullYear();
 
 
 
-    // $(function() {
-    // var params = {
-    //     // Request parameters
-    // };
+    $(function() {
+    var params = {
+        // Request parameters
+    };
     
-    // $.ajax({
-    //     url: "https://api.fantasydata.net/v3/nba/scores/{format}/Stadiums?" + $.param(params),
-    //     beforeSend: function(xhrObj){
-    //         // Request headers
-    //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key",subscriptionKey);
-    //     },
-    //     type: "GET",
-    //     // Request body
-    //     data: "{body}",
-    // })
-    // .done(function(data) {
-    //     console.log("TEAM STADIUM success");
-    //     console.log(data);
-    // })
-    // .fail(function() {
-    //     console.log("error");
-    // });
-    // });
+    $.ajax({
+        url: "https://api.fantasydata.net/v3/nba/scores/JSON/Stadiums?" + $.param(params),
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key",subscriptionKey);
+        },
+        type: "GET",
+        // Request body
+        data: "{body}",
+    })
+    .done(function(data) {
+        console.log("TEAM STADIUM success");
+        console.log(data);
+
+
+        
+        for(var i = 0; i < 30; i++) {
+        
+            console.log(data[i].State)
+        var nbaLat = (data[6].GeoLat)
+        var nbaLong = (data[6].GeoLong)
+    
+
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: nbaLat, lng: nbaLong},
+              zoom: 13
+          
+          
+          
+            });
+          
+            var input = document.getElementById('pac-input');
+          
+            var autocomplete = new google.maps.places.Autocomplete(input);
+            autocomplete.bindTo('bounds', map);
+          
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+          
+            var infowindow = new google.maps.InfoWindow();
+            var infowindowContent = document.getElementById('infowindow-content');
+            infowindow.setContent(infowindowContent);
+            var marker = new google.maps.Marker({
+              map: map
+            });
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+          
+            autocomplete.addListener('place_changed', function() {
+              infowindow.close();
+              var place = autocomplete.getPlace();
+              if (!place.geometry) {
+                return;
+              }
+          
+              if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+              } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);
+              }
+          
+              // Set the position of the marker using the place ID and location.
+              marker.setPlace({
+                placeId: place.place_id,
+                location: place.geometry.location
+              });
+              marker.setVisible(true);
+          
+              infowindowContent.children['place-name'].textContent = place.name;
+              infowindowContent.children['place-id'].textContent = place.place_id;
+              infowindowContent.children['place-address'].textContent =
+                  place.formatted_address;
+              infowindow.open(map, marker);
+            });
+          }
+          
+          initMap()
+
+
+
+
+
+        }
+    })
+
+    .fail(function() {
+        console.log("error");
+    });
+    });
 
 
     //API PULL FOR ALL NBA TEAMS AND INFO
@@ -185,6 +264,12 @@ var yyyy = today.getFullYear();
         //on click function
         $(document).on('click','.logos',function(){
             console.log($(this).data('team'))
+
+           
+
+
+
+
         })
         
         
@@ -259,59 +344,62 @@ var yyyy = today.getFullYear();
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:  */
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13
-  });
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: nbaLat, lng: nbaLong},
+//     zoom: 13
 
-  var input = document.getElementById('pac-input');
 
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//   });
 
-  var infowindow = new google.maps.InfoWindow();
-  var infowindowContent = document.getElementById('infowindow-content');
-  infowindow.setContent(infowindowContent);
-  var marker = new google.maps.Marker({
-    map: map
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
+//   var input = document.getElementById('pac-input');
 
-  autocomplete.addListener('place_changed', function() {
-    infowindow.close();
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      return;
-    }
+//   var autocomplete = new google.maps.places.Autocomplete(input);
+//   autocomplete.bindTo('bounds', map);
 
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
+//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-    // Set the position of the marker using the place ID and location.
-    marker.setPlace({
-      placeId: place.place_id,
-      location: place.geometry.location
-    });
-    marker.setVisible(true);
+//   var infowindow = new google.maps.InfoWindow();
+//   var infowindowContent = document.getElementById('infowindow-content');
+//   infowindow.setContent(infowindowContent);
+//   var marker = new google.maps.Marker({
+//     map: map
+//   });
+//   marker.addListener('click', function() {
+//     infowindow.open(map, marker);
+//   });
 
-    infowindowContent.children['place-name'].textContent = place.name;
-    infowindowContent.children['place-id'].textContent = place.place_id;
-    infowindowContent.children['place-address'].textContent =
-        place.formatted_address;
-    infowindow.open(map, marker);
-  });
-}
+//   autocomplete.addListener('place_changed', function() {
+//     infowindow.close();
+//     var place = autocomplete.getPlace();
+//     if (!place.geometry) {
+//       return;
+//     }
 
-initMap()
+//     if (place.geometry.viewport) {
+//       map.fitBounds(place.geometry.viewport);
+//     } else {
+//       map.setCenter(place.geometry.location);
+//       map.setZoom(17);
+//     }
+
+//     // Set the position of the marker using the place ID and location.
+//     marker.setPlace({
+//       placeId: place.place_id,
+//       location: place.geometry.location
+//     });
+//     marker.setVisible(true);
+
+//     infowindowContent.children['place-name'].textContent = place.name;
+//     infowindowContent.children['place-id'].textContent = place.place_id;
+//     infowindowContent.children['place-address'].textContent =
+//         place.formatted_address;
+//     infowindow.open(map, marker);
+//   });
+// }
+
+// initMap()
 
  
 
